9497d335a1f971c6e019857e6365febd3a92bc08
dan.castillo <dancastillo99@gmail.com>
2024-11-28 21:30:18 -0500

    chore: clean up response data

--- src/lib/api/contribution/contribution.ts
+++ src/lib/api/contribution/contribution.ts
@@ -7,17 +7,14 @@ export async function getContributions(): Promise<Contribution[]> {
     const apiUrl = getEnv(ENV_VARS.API_URL)
     const route = ROUTES.CONTRIBUTIONS
 
-    console.log('${apiUrl}${route}', `${apiUrl}${route}`)
-    const res = await fetch(`${apiUrl}${route}`)
-    const data = (await res.json()) as ContributionResponse
+    const response = await fetch(`${apiUrl}${route}`)
+    const json = (await response.json()) as ContributionResponse
 
-    if (data.errors.length > 0) {
-      console.error('Error fetching contributions:', data.errors)
+    if (json.errors.length > 0) {
       return []
     }
 
-    console.log('data', data)
-    return data.data.map((contribution: ContributionData): Contribution => mapContribution(contribution))
+    return json.data.map((contribution: ContributionData): Contribution => mapContribution(contribution))
   } catch (err) {
     console.error('Error fetching contributions:', err)
     return []
--- src/lib/api/post/post.ts
+++ src/lib/api/post/post.ts
@@ -1,45 +1,20 @@
 import { ENV_VARS, getEnv } from '../env'
 import { ROUTES } from '../routes'
-import { mapPost, Post, PostData } from './post.mapper'
-import path from 'node:path'
-import { remark } from 'remark'
-import html from 'remark-html'
-import { readFile, readdir } from 'fs/promises'
-
-const postDir = path.join(process.cwd(), 'src/posts')
+import { mapPost, Post, PostData, PostResponse } from './post.mapper'
 
 export async function getPosts(): Promise<Post[]> {
   try {
-    if (getEnv(ENV_VARS.LOCAL_DEVELOPMENT) === 'true') {
-      const fileNames = await readdir(postDir)
-      const posts = await Promise.all(
-        fileNames.map(async (fileName): Promise<Post> => {
-          const filePath = path.join(postDir, fileName)
-          const fileContents = await readFile(filePath, 'utf8')
-
-          const processedContent = await remark().use(html).process(fileContents)
-
-          const content = processedContent.toString()
-          return {
-            id: 'some-random-uuid-id-here',
-            title: 'Some Title',
-            date: new Date().toISOString(),
-            content: content,
-            tags: [],
-          }
-        })
-      )
-
-      return posts as Post[]
-    }
-
     const apiUrl = getEnv(ENV_VARS.API_URL)
     const route = ROUTES.POSTS
 
-    const res = await fetch(`${apiUrl}${route}`)
-    const data = (await res.json()) as PostData[]
+    const response = await fetch(`${apiUrl}${route}`)
+    const json = (await response.json()) as PostResponse
 
-    return data.map((post: PostData) => mapPost(post))
+    if (json.errors.length > 0) {
+      return []
+    }
+
+    return json.data.map((post: PostData) => mapPost(post))
   } catch (err) {
     console.error('Error fetching posts:', err)
     return []
